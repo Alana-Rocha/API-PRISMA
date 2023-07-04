42146131a873782b7c0727b38829eb4ecc71c653
Alana-Rocha <alanarodrigues.rocha1@gmail.com>
2023-07-04 09:44:41 -0300

    adding post

---
+++ src/posts/repositories/posts.repository.ts
@@ -0,0 +1,69 @@
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreatePostDto } from '../dto/create-post.dto';
+import { UpdatePostDto } from '../dto/update-post.dto';
+import { PostEntity } from '../entities/post.entity';
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { NotFoundError } from 'src/common/errors/types/NotFoundError';
+
+@Injectable()
+export class PostRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createPostDto: CreatePostDto): Promise<PostEntity> {
+    const { authorEmail } = createPostDto;
+
+    delete createPostDto.authorEmail;
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: authorEmail,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundError('Author not found.');
+    }
+
+    const data: Prisma.PostCreateInput = {
+      ...createPostDto,
+      author: {
+        connect: {
+          email: authorEmail,
+        },
+      },
+    };
+    return await this.prisma.post.create({
+      data,
+    });
+  }
+
+  async findAll(): Promise<PostEntity[]> {
+    return await this.prisma.post.findMany();
+  }
+
+  async findOne(id: number): Promise<PostEntity> {
+    return await this.prisma.post.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: number, updatePostDto: UpdatePostDto): Promise<PostEntity> {
+    return this.prisma.post.update({
+      where: {
+        id,
+      },
+      data: updatePostDto,
+    });
+  }
+
+  remove(id: number): Promise<PostEntity> {
+    return this.prisma.post.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
